8028e0028f9dccf5bb52c03835459c444ddec018
Eagle <eaglenguyen1@gmail.com>
2025-12-15 18:41:00 -0500

    12/15 - Added logic for populating search list with restaurant. Calling viewmodel method intit

--- lib/ui/search_screen.dart
+++ lib/ui/search_screen.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:foodaroundme/viewmodel/mapViewModel.dart';
+import 'package:provider/provider.dart';
 
 
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<MapViewModel>().loadNearbyRestaurants("restaurant");
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MapViewModel>();
     return Scaffold(
       appBar: AppBar(title: const Text("")),
       body: Stack(
         children: [
-          const Center(child: Text("Search page content")),
+
+
+          // 📍 Restaurant list
+          ListView.builder(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 160),
+            itemCount: viewModel.filteredPlaces.length,
+            itemBuilder: (_, index) {
+              final place = viewModel.filteredPlaces[index];
+
+              return ListTile(
+                leading: const Icon(Icons.restaurant),
+                title: Text(place.name),
+                subtitle: Text(
+                  "1 mile away",
+                ),
+              );
+            },
+          ),
 
           // 🔍 Bottom search bar
           Positioned(
